8c173fa73d1e3ab90322cfe2a19f86bd669706b5
Louisiane Lemaire <louisiane.lemaire@inria.fr>
2025-08-04 15:06:38 +0200

    fix issue auto directory

--- bd_wrt_gna_hu.py
+++ bd_wrt_gna_hu.py
@@ -15,13 +15,12 @@ def compute_bd(i_app_val):
     param_set_id = "wt"
     g_na_max = hu.params_default["g_na_tot"]
 
-    # fixed point, will be used as starting point of the continuation
-    prepare_continuation_hu.compute_fp(param_set_id)
-    inits = auto_helper.load_fixed_point(auto_dir, param_set_id)
-
     # directory with the auto files
     auto_dir = auto_helper.compute_auto_dir(model_name)
 
+    # fixed point, will be used as starting point of the continuation
+    prepare_continuation_hu.compute_fp(param_set_id)
+    inits = auto_helper.load_fixed_point(auto_dir, param_set_id)
 
     ########################## continuation ##########################
     auto_helper.run(filename=f"branch_eq_wrt_i_app_ends_{i_app_val}", auto_dir=auto_dir, output_dir="wt", e=model_name, c=model_name,
@@ -39,8 +38,8 @@ def compute_bd(i_app_val):
     #### branch of limit cycles ####
     # dummy (to remove first few points)
     auto_helper.run(filename=f"branch_lc_wrt_gnatot_i_app_{i_app_val}_dummy", auto_dir=auto_dir, output_dir="wt", e=model_name, c=model_name,
-                s=f"branch_eq_wrt_gnatot_i_app_{i_app_val}", IRS="HB1", ICP=["GNATOT", "PERIOD"], UZSTOP={"GNATOT": [0, g_na_max], "PERIOD": 400}, IPLT=-1, IPS=2, STOP=["BP1", "LP10"], NPR=1, NMX=5)
+                s=f"branch_eq_wrt_gnatot_i_app_{i_app_val}", IRS="HB1", ICP=["GNATOT", "PERIOD"], UZSTOP={"GNATOT": [0, g_na_max], "PERIOD": 400}, IPLT=-1, IPS=2, STOP=["BP1", "LP10"], NMX=5)
 
     # actual run
     auto_helper.run(filename=f"branch_lc_wrt_gnatot_i_app_{i_app_val}", auto_dir=auto_dir, output_dir="wt", e=model_name, c=model_name,
-                s=f"branch_lc_wrt_gnatot_i_app_{i_app_val}_dummy", IRS="EP1", ICP=["GNATOT", "PERIOD"], UZSTOP={"GNATOT": [0, g_na_max], "PERIOD": 400}, IPLT=-1, IPS=2, STOP=["BP1", "LP10"], NPR=1)
+                s=f"branch_lc_wrt_gnatot_i_app_{i_app_val}_dummy", IRS="EP1", ICP=["GNATOT", "PERIOD"], UZSTOP={"GNATOT": [0, g_na_max], "PERIOD": 400}, IPLT=-1, IPS=2, STOP=["BP1", "LP10"])
